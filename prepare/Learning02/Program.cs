0b9cebecc6df42e2a19ba33f144d9179219959eb
mtmenji <mtmenji@gmail.com>
2023-01-16 10:12:27 -0700

    W03 Prepare Module

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2018;
+        job1._endYear = 2020;
+        job1.Display();
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "UX Designer";
+        job2._startYear = 2021;
+        job2._endYear = 2023;
+        job2.Display();
+
+        Resume resume = new Resume();
+        resume._name = "Michael Menjivar";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+        resume.Display();
     }
 }
